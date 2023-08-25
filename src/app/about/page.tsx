0548da26be4c736d7929d64bcd657f7e2ac9d6dd
Collins Kiprono Kirui <101986591+iamkiprono@users.noreply.github.com>
2023-08-25 16:15:29 +0300

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,21 +7,21 @@ const page = () => {
       <p className="text-xl font-bold mb-4">About Me</p>
       <div className="p-6 border-l-[1px]">
         <p>
-          I'm a passionate MERN stack developer with a flair for crafting
+          I am a passionate MERN stack developer with a flair for crafting
           remarkable web experiences. My journey into the world of coding began
           with a fascination for technology and a drive to create innovative
           solutions. Armed with Next.js and React, I wield the power of these
           frameworks to breathe life into captivating frontends that leave users
           in awe. As a lover of organized data, SQL databases are my trusty
           companions, helping me tame the chaos and maintain seamless
-          operations. But I don't just stop there – I make sure my creations
+          operations. But I dont just stop there  I make sure my creations
           shine with the elegance of Tailwind CSS, turning designs into elegant
-          realities. When I'm not weaving code, you can find me immersed in a
-          world of music, exploring nature's wonders, or enjoying
+          realities. When I am not weaving code, you can find me immersed in a
+          world of music, exploring wonders of nature, or enjoying
           thought-provoking books. I believe in the power of continuous learning
           and constantly seek new challenges to sharpen my skills and expand my
           horizons. Join me on this extraordinary journey of transforming ideas
-          into reality, one line of code at a time. Let's build something
+          into reality, one line of code at a time. Let us build something
           incredible together!
         </p>
       </div>
